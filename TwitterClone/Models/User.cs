89ab0ed8b6881e5b86ff210211865ef70badabc3
Ashraf Muhamad <ashrafmuhamad2014@gmail.com>
2023-08-23 16:46:02 +0300

    created controller dbcontext model of user

---
+++ TwitterClone/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TwitterClone.Models
+{
+    public class User
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Picture { get; set; }
+        public string [] Followers { get; set; }
+        public string [] Following { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public User() { }
+        
+    }
+}
--- TwitterClone/Program.cs
+++ TwitterClone/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TwitterClone.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
