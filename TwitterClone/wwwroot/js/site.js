277b7ee0f4984f9d49ffadafbcbe738263b6a065
nejishow <ashrafmuhamad2014@gmail.com>
2023-09-03 05:11:56 +0300

    finish tweet template

--- TwitterClone/wwwroot/js/site.js
+++ TwitterClone/wwwroot/js/site.js
@@ -1,4 +1,19 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Dealing with Input width
+let el = document.querySelector(".input-wrap .input");
+let widthMachine = document.querySelector(".input-wrap .width-machine");
+el.addEventListener("keyup", () => {
+    widthMachine.innerHTML = el.value;
+});
 
-// Write your JavaScript code.
+// Dealing with Textarea Height
+function calcHeight(value) {
+    let numberOfLineBreaks = (value.match(/\n/g) || []).length;
+    // min-height + lines x line-height + padding + border
+    let newHeight = 20 + numberOfLineBreaks * 20 + 12 + 2;
+    return newHeight;
+}
+
+let textarea = document.querySelector(".resize-ta");
+textarea.addEventListener("keyup", () => {
+    textarea.style.height = calcHeight(textarea.value) + "px";
+});
